495c9677d6bf7af205d3dce407542f2c1e2c3ed8
Mengmei Tu <redmmeii@gmail.com>
2020-12-31 20:37:19 -0600

    components outline setup

---
+++ src/RollDice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RollDice extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default RollDice;
