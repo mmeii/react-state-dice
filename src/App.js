a0d1f3b724240ef91b71064137f191a7d2fe9be3
Mengmei Tu <redmmeii@gmail.com>
2021-01-02 17:03:41 -0600

    set up rolldice.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Die face="five" />
-        <Die face="one" />
         <RollDice />
       </div>
     );
--- src/RollDice.js
+++ src/RollDice.js
@@ -8,17 +8,32 @@ class RollDice extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { die1: "one", die2: "one", rolling: false }
+        this.state = { die1: "one", die2: "one", rolling: false };
+        this.roll = this.roll.bind(this);
     }
 
     roll() {
-        let random = Math.floor(Math.random())
+        //pick 2 new rolls
+        const newDie1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        const newDie2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+
+        //set state with new rolls
+        this.setState({ die1: newDie1, die2: newDie2 })
     }
     render() {
         return (
             <div>
-
+                <Die face={this.state.die1} />
+                <Die face={this.state.die2} />
+                <button onClick={this.roll}>
+                    Roll Dice!
+                </button>
             </div>
+
         )
     }
 }
